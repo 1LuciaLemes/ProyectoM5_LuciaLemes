b90aba82ccc7bee11c9c0663c848bb2d6d9bd1f0
Lucia Lemes <lemeslucya@gmail.com>
2026-07-07 16:52:38 -0300

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type {JSX} from "react";
+
+function App(): JSX.Element {
+  return (
+    <div style={{padding: "2rm"}}>
+      <h1>HENRY-Commerce</h1>
+      </div>
+  )
+}
+
+export default App
